d482b6fb9b64c88c31f001b5e2d4beee3c341877
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-10-11 22:15:36 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,51 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  // nueva carta
+  const generarCarta = () => {
+    let numero = generarRandomNumero();
+    let pinta = generarRandomPinta();
+    let cardElement = document.querySelector(".card");
 
-window.onload = function() {
-  //write your code here
+    cardElement.className = "card " + pinta;
+    cardElement.innerHTML = numero;
+  };
+
+  //  número aleatorio
+  let generarRandomNumero = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let iNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[iNumbers];
+  };
+
+  // pinta aleatoria
+  let generarRandomPinta = () => {
+    let pintas = ["heart", "club", "diamond", "spade"];
+    let iPintas = Math.floor(Math.random() * pintas.length);
+    return pintas[iPintas];
+  };
+
+  //  carta al cargar la página cada 10 segundos
+  generarCarta();
+  setInterval(generarCarta, 10000);
+
+  //boton
+  document.getElementById("generateCardBtn").addEventListener("click", () => {
+    generarCarta();
+  });
 };
